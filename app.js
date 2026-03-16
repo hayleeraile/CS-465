c7a44113505028f3cf6461410da5e6e01a566a6f
Haylee Raile <hayleeraile@gmail.com>
2026-03-16 15:59:25 -0700

    Module 3 completed baseline

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var roomsRouter = require('./app_server/routes/rooms');
+var newsRouter = require('./app_server/routes/news');
+var mealsRouter = require('./app_server/routes/meals');
+var contactRouter = require('./app_server/routes/contact');
+var aboutRouter = require('./app_server/routes/about');
 var handlebars = require('hbs');
 
 var app = express();
@@ -27,6 +32,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/rooms', roomsRouter)
+app.use('/news', newsRouter);
+app.use('/meals', mealsRouter);
+app.use('/contact', contactRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app_server/controllers/meals.js
@@ -0,0 +1,9 @@
+
+/* GET meals view */
+const meals = (req, res) => {
+    res.render('meals', { title: 'Travlr Getaways'});
+};
+
+module.exports = {
+    meals
+};
